173310bfdab3d0f844b3ff5a29df972a0fc9edbe
ayon121 <borshonsaha1234@gmail.com>
2025-09-16 11:17:41 +0600

    user delete and update function completed

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -7,6 +7,14 @@ export const Userrouter = express.Router()
 Userrouter.get("/" , UserController.gettAllUser)
 Userrouter.get("/:id" , UserController.getUserById)
 Userrouter.post("/" , UserController.createUser)
+Userrouter.patch(
+    "/:id",
+    UserController.updateUser
+)
+Userrouter.delete(
+    "/:id",
+    UserController.deleteUser
+)
 
 
 
